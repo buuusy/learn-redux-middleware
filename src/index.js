2a4deb5a537be3500b5a848d4dbccdca0ca81bab
buuusy <dev.c.jade@gmail.com>
2022-07-28 14:26:51 +0900

    redux-logger 라이브러리 적용

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createStore, applyMiddleware } from 'redux';
-import loggerMiddleware from './library/loggerMiddleware.js';
+//import loggerMiddleware from './library/loggerMiddleware.js';
+import { createLogger } from 'redux-logger';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 root.render(
   <Provider store={store}>
     <App />
