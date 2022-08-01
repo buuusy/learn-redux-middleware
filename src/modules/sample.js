07406761c283504689fc17bc23e5186654a395aa
buuusy <dev.c.jade@gmail.com>
2022-08-01 15:18:53 +0900

    코드 리팩토링

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from 'redux-actions';
 import * as api from '../library/api';
+import createRequestThunk from '../library/createRequestThunk';
 
 //액션타입 정의
 //한 요청당 3개를 만들어야함.
@@ -13,37 +14,8 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 //thunk 함수(미들웨어)생성. thunk 함수 내부에서는 시작할때, 성공했을때, 실패했을때 다른 액션을 디스패치함
 
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST }); //요청을 시작한것을 알림
-  try {
-    const response = await api.getPost(id);
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data,
-    }); // 요청성공
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    }); //에러발생
-    throw e; //나중에 컴포넌트단에서 에러를 조회할 수 있게 해 줌.
-  }
-};
-
-export const getUsers = () => async (dispatch) => {
-  dispatch({ type: GET_USERS }); //요청을 시작한것을 알림
-  try {
-    const response = await api.getUsers();
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({ type: GET_USERS_FAILURE, payload: e, error: true });
-    throw e;
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 const initailaState = {
   loading: {
